104646f330288f9c52806e34bb3786ac9d48eac8
ianicbrisson <49887904+ianicbrisson@users.noreply.github.com>
2019-04-22 20:42:21 -0400

    Add files via upload

---
+++ boot_workspace.js
@@ -0,0 +1,20 @@
+// This code should be pasted into the ChiliPeppr Edit Boot Javascript dialog box
+// located in the upper right corner of any chilipeppr.com page.
+// The ChiliPeppr environment has a standard div called #pnlWorkspace that
+// this workspace should be loaded into.
+chilipeppr.load(
+  "#pnlWorkspace",
+  "http://raw.githubusercontent.com/ianic-cnc/workspace-grbl1/master/auto-generated-workspace.html",
+  function() {
+    // Callback after workspace loaded into #pnlWorkspace
+    // Now use require.js to get reference to instantiated workspace
+    cprequire(
+      ["inline:com-chilipeppr-workspace-grbl"], // the id you gave your workspace
+      function(myWorkspaceGrbl) {
+        // Callback that is passed reference to the newly loaded workspace
+        console.log("Workspace / grbl just got loaded.", myWorkspaceGrbl);
+        myWorkspaceGrbl.init();
+      }
+    );
+  }
+);
